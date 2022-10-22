358d323f8f0a35cc3f2422cb41104b8f03ecf69f
Luca <Luca@hackerspace-bamberg.de>
2022-10-22 19:39:04 +0200

    Update year

--- cluequiz/config.py
+++ cluequiz/config.py
@@ -1,5 +1,5 @@
 # Clue quiz
-# Copyright (C) 2018-2021  Luca Schmid
+# Copyright (C) 2018-2022  Luca Schmid
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
